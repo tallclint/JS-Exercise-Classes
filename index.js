415618c6acc7f110a3b007a6b00812a5304904b8
tallclint <87029337+tallclint@users.noreply.github.com>
2021-08-19 17:04:57 -0600

    task 5

--- index.js
+++ index.js
@@ -172,9 +172,25 @@ gradeUpdate(){
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-   
-}
+class Student  extends Lambdasian {
+  constructor(atters){
+    super(atters);
+    this.grade = atters.grade;
+    this.previousBackground = atters.previousBackground ;
+    this.className = atters.className ;
+    this.favSubjects = atters.favSubjects;
+  }
+  listSubjects(){
+    return `Loving ${this.favSubjects}!`;
+  }
+  PRAssignment(subject) {
+   return `${this.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
+  }
+
 
 /*
   TASK 6
